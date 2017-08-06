af4a5598e4e395a0f78cbd015f234cce36183aec
beidah <beidah@gmail.com>
2017-08-06 00:04:27 -0600

    Add Key input

--- Chip8/main.cpp
+++ Chip8/main.cpp
@@ -14,8 +14,11 @@ int display_width = SCREEN_WIDTH * modifier;
 int display_height = SCREEN_HEIGHT * modifier;
 
 void drawPixels(SDL_Renderer* renderer);
+void keydown(const SDL_KeyboardEvent& key);
+void keyup(const SDL_KeyboardEvent& key);
 
 #undef main
+
 
 int main(int argc, char** argv)
 {
@@ -29,7 +32,7 @@ int main(int argc, char** argv)
 	if (!myChip8.loadProgram(argv[1]))
 		return 1;
 
-	myChip8.loadProgram("./pong.rom");
+	// myChip8.loadProgram("./pong.rom");
 
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
@@ -60,6 +63,17 @@ int main(int argc, char** argv)
 		if (SDL_PollEvent(&e))
 		{
 			if (e.type == SDL_QUIT) break;
+
+			if (e.type == SDL_KEYDOWN)
+			{
+				auto key = e.key;
+				keydown(key);
+			}
+			else if (e.type == SDL_KEYUP)
+			{
+				auto key = e.key;
+				keyup(key);
+			}
 		}
 
 		myChip8.emulateCycle();
@@ -70,7 +84,7 @@ int main(int argc, char** argv)
 		}
 
 		SDL_RenderPresent(renderer);
-		SDL_Delay(1000 / 60);
+		// SDL_Delay(1000 / 60);
 	}
 
 	SDL_DestroyRenderer(renderer);
@@ -97,5 +111,124 @@ void drawPixels(SDL_Renderer* renderer)
 			rect.h = modifier;
 			SDL_RenderFillRect(renderer, &rect);
 		}
+	}
+}
+
+void keydown(const SDL_KeyboardEvent& key)
+{
+	switch (key.keysym.scancode)
+	{
+	case SDL_SCANCODE_ESCAPE:
+		exit(0);
+
+	case SDL_SCANCODE_1:
+		myChip8.key[0x1] = 1;
+		break;
+	case SDL_SCANCODE_2:
+		myChip8.key[0x2] = 1;
+		break;
+	case SDL_SCANCODE_3:
+		myChip8.key[0x3] = 1;
+		break;
+	case SDL_SCANCODE_4:
+		myChip8.key[0xC] = 1;
+		break;
+	case SDL_SCANCODE_Q:
+		myChip8.key[0x4] = 1;
+		break;
+	case SDL_SCANCODE_W:
+		myChip8.key[0x5] = 1;
+		break;
+	case SDL_SCANCODE_E:
+		myChip8.key[0x6] = 1;
+		break;
+	case SDL_SCANCODE_R:
+		myChip8.key[0xD] = 1;
+		break;
+	case SDL_SCANCODE_A:
+		myChip8.key[0x7] = 1;
+		break;
+	case SDL_SCANCODE_S:
+		myChip8.key[0x8] = 1;
+		break;
+	case SDL_SCANCODE_D:
+		myChip8.key[0x9] = 1;
+		break;
+	case SDL_SCANCODE_F:
+		myChip8.key[0xE] = 1;
+		break;
+	case SDL_SCANCODE_Z:
+		myChip8.key[0xA] = 1;
+		break;
+	case SDL_SCANCODE_X:
+		myChip8.key[0x0] = 1;
+		break;
+	case SDL_SCANCODE_C:
+		myChip8.key[0xB] = 1;
+		break;
+	case SDL_SCANCODE_V:
+		myChip8.key[0xF] = 1;
+		break;
+
+	default:
+		break;
+	}
+}
+
+void keyup(const SDL_KeyboardEvent& key)
+{
+	switch (key.keysym.scancode)
+	{
+
+	case SDL_SCANCODE_1:
+		myChip8.key[0x1] = 0;
+		break;
+	case SDL_SCANCODE_2:
+		myChip8.key[0x2] = 0;
+		break;
+	case SDL_SCANCODE_3:
+		myChip8.key[0x3] = 0;
+		break;
+	case SDL_SCANCODE_4:
+		myChip8.key[0xC] = 0;
+		break;
+	case SDL_SCANCODE_Q:
+		myChip8.key[0x4] = 0;
+		break;
+	case SDL_SCANCODE_W:
+		myChip8.key[0x5] = 0;
+		break;
+	case SDL_SCANCODE_E:
+		myChip8.key[0x6] = 0;
+		break;
+	case SDL_SCANCODE_R:
+		myChip8.key[0xD] = 0;
+		break;
+	case SDL_SCANCODE_A:
+		myChip8.key[0x7] = 0;
+		break;
+	case SDL_SCANCODE_S:
+		myChip8.key[0x8] = 0;
+		break;
+	case SDL_SCANCODE_D:
+		myChip8.key[0x9] = 0;
+		break;
+	case SDL_SCANCODE_F:
+		myChip8.key[0xE] = 0;
+		break;
+	case SDL_SCANCODE_Z:
+		myChip8.key[0xA] = 0;
+		break;
+	case SDL_SCANCODE_X:
+		myChip8.key[0x0] = 0;
+		break;
+	case SDL_SCANCODE_C:
+		myChip8.key[0xB] = 0;
+		break;
+	case SDL_SCANCODE_V:
+		myChip8.key[0xF] = 0;
+		break;
+
+	default: break;
 	}
 }
